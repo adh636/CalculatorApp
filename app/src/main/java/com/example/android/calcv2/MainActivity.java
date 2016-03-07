a3f6a7e81647bd7c750fcb158589586409068268
Adam <adh636@gmail.com>
2016-03-07 07:16:09 -0500

    add comments and reorganize code

--- app/src/main/java/com/example/android/calcv2/MainActivity.java
+++ app/src/main/java/com/example/android/calcv2/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,71 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private String operation = "";
     private boolean reset = true;
     private boolean equal = false;
-    private boolean lastPressed = true; // ensures the last button pressed wasn't an operator
-    
-    public void updateNumber(String number) {
-        if (!reset) {
-            resetAll();
-        }
-        if (currentNumber.equals("") && operation.equals("")) {
-            display = number;
-            currentNumber = number;
-            showDisplay();
-        }
-        else {
-            display += number;
-            currentNumber += number;
-            showDisplay();
-        }
-        lastPressed = false;
-    }
-
-    public void addModifier(String modifier) {
-        if (lastPressed) {
-            return;
-        }
-        if (!previousNumber.equals("") && !equal) {
-            previousNumber = Double.toString(calculateResult());
-        }
-        else if (previousNumber.equals("")){
-            previousNumber = currentNumber;
-        }
-        operation = modifier;
-        display += " " + operation + " ";
-        currentNumber = "";
-        showDisplay();
-        reset = true;
-        equal = false;
-        lastPressed = true;
-    }
-
-    public double calculateResult() {
-        if (operation.equals("+")) {
-            return Double.parseDouble(previousNumber) + Double.parseDouble(currentNumber);
-        }
-        else if (operation.equals("-")) {
-            return Double.parseDouble(previousNumber) - Double.parseDouble(currentNumber);
-        }
-        else if (operation.equals("*")) {
-            return Double.parseDouble(previousNumber) * Double.parseDouble(currentNumber);
-        }
-        else if (operation.equals("/")) {
-            return Double.parseDouble(previousNumber) / Double.parseDouble(currentNumber);
-        }
-        else {
-            return Double.parseDouble(currentNumber);
-        }
-    }
-
-    public void resetAll() {
-        display = "0";
-        currentNumber = "";
-        previousNumber = "";
-        operation = "";
-        reset = true;
-        equal = false;
-        lastPressed = true;
-    }
+    private boolean lastPressed = true; // tells you if last button pressed was an operator
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,25 +46,128 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showDisplay() {
-        TextView calcDisplay = (TextView) findViewById(R.id.display);
-        ScrollView scrollDisplay = (ScrollView) findViewById(R.id.scroller);
-        calcDisplay.setText(String.valueOf(display));
-        scrollDisplay.fullScroll(View.FOCUS_DOWN);
-    }
-    
-    public void equal(View v) {
-        display += " = " + calculateResult();
-        previousNumber = Double.toString(calculateResult());
-        reset = false;
-        equal = true;
-        int length = display.length();
-        if (display.substring(length - 2, length).equals(".0")) {
-            display = display.substring(0, length - 2);
+    // executes whenever a number or decimal point is pressed
+    public void updateNumber(String number) {
+
+        // if last button pressed was "=", resets before updating number
+        if (!reset) {
+            resetAll();
         }
+
+        //  displays and stores first number if variables have been reset or app just loaded
+        if (currentNumber.equals("") && operation.equals("")) {
+            display = number;
+            currentNumber = number;
+            showDisplay();
+        }
+
+        // appends number to display and current number
+        else {
+            display += number;
+            currentNumber += number;
+            showDisplay();
+        }
+
+        // allows an operator to be used
+        lastPressed = false;
+    }
+
+    // executes whenever a modifier is pressed (+, -, *, /)
+    public void addModifier(String modifier) {
+
+        // do nothing if a number or equal wasn't last button pressed
+        if (lastPressed) {
+            return;
+        }
+        
+        // if previousNumber is not empty and equal wasn't last button pressed, 
+        // calculator result and update previousNumber
+        if (!previousNumber.equals("") && !equal) {
+            previousNumber = Double.toString(calculateResult());
+        }
+        
+        // if previousNumber is not empty and equal was pressed last,
+        // store currentNumber in previousNumber
+        else if (previousNumber.equals("")){
+            previousNumber = currentNumber;
+        }
+        
+        
+        operation = modifier; // update operation with modifier
+        display += " " + operation + " "; // update display with modifier
+        currentNumber = ""; // clear currentNumber
+        reset = true; // keeps display from resetting
+        equal = false; // stores that equal was not last button pressed
+        lastPressed = true; // stores last pressed as operator
         showDisplay();
     }
     
+    // return solution based on which operator was pressed last
+    public double calculateResult() {
+        if (operation.equals("+")) {
+            return Double.parseDouble(previousNumber) + Double.parseDouble(currentNumber);
+        }
+        else if (operation.equals("-")) {
+            return Double.parseDouble(previousNumber) - Double.parseDouble(currentNumber);
+        }
+        else if (operation.equals("*")) {
+            return Double.parseDouble(previousNumber) * Double.parseDouble(currentNumber);
+        }
+        else if (operation.equals("/")) {
+            return Double.parseDouble(previousNumber) / Double.parseDouble(currentNumber);
+        }
+        else {
+            return Double.parseDouble(currentNumber);
+        }
+    }
+    
+    // set all variables to default values
+    public void resetAll() {
+        display = "0";
+        currentNumber = "";
+        previousNumber = "";
+        operation = "";
+        reset = true;
+        equal = false;
+        lastPressed = true;
+    }
+    
+    // display text on app screen
+    public void showDisplay() {
+        TextView calcDisplay = (TextView) findViewById(R.id.display);
+        ScrollView scrollDisplay = (ScrollView) findViewById(R.id.scroller);
+        calcDisplay.setText(String.valueOf(display)); // set text in TextView to content stored in display
+        scrollDisplay.fullScroll(View.FOCUS_DOWN); // scroll the view to the bottom
+    }
+
+    // executes when equal button is pressed
+    public void equal(View v) {
+
+        // do nothing if a number or equal wasn't last button pressed
+        if (lastPressed) {
+            return;
+        }
+        
+        display += " = " + calculateResult(); // update display with equal sign and result
+        previousNumber = Double.toString(calculateResult()); // update previousNumber
+        reset = false; // if a number is pressed next, variable are reset
+        equal = true; // stores that equal was last button pressed
+        int length = display.length();
+        
+        // if calculateResult() returned ".0" at the end, remove from display
+        if (display.substring(length - 2, length).equals(".0")) {
+            display = display.substring(0, length - 2);
+        }
+        
+        showDisplay();
+    }
+    
+    // reset variables and display
+    public void clear(View v) {
+        resetAll();
+        showDisplay();
+    }
+
     // implementation not yet correct (not updating display)
     /*
     public void plusMinus(View v) {
@@ -137,8 +177,6 @@ public class MainActivity extends AppCompatActivity {
         else {
             currentNumber = "-" + currentNumber;
         }
-        display += currentNumber
-        showDisplay();
     }
     */
 
@@ -200,10 +238,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void divide(View v) {
         addModifier("/");
-    }
-
-    public void clear(View v) {
-        resetAll();
-        showDisplay();
     }
 }
